5c3d0fc645edcfde7831195de65608cabe44aa40
agiledivops <agile.devops@protonmail.com>
2024-10-18 23:34:25 +0500

    Update Configuration.php
    namespace update

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace FourLabs\GampBundle\DependencyInjection;
+namespace AgileDivops\GampBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
